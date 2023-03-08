91bb012e6f577331d9da1683a2f1e65a5b29b589
Glenn <gcrosby15@gmail.com>
2023-03-09 00:05:32 +0700

    Bad commit - ignore

--- app_api/controllers/locs_api_ctrl.py
+++ app_api/controllers/locs_api_ctrl.py
@@ -50,41 +50,42 @@ def location_create(request):
 			coords = [location_data['lng'], location_data['lat']]
 		)
 
-		# add the opening time sub documents:
-		opening_time_records = list()
-		for opening_time in location_data['openingTimes']:
-			print("opening_time= {}".format(opening_time))
+		# # add the opening time sub documents:
+		# opening_time_records = list()
+		# for opening_time in location_data['openingTimes']:
+		# 	print("opening_time= {}".format(opening_time))
 
-			# check required data days and closed:
-			if not opening_time.get('days'):
-				print("Error opeing_time['days']")
-				response = ({'message': "{}.opening_times is invalide. Missing days.".format(location['name'])}, 400)
-				break
+		# 	# check required data days and closed:
+		# 	if not opening_time.get('days'):
+		# 		print("Error opeing_time['days']")
+		# 		response = ({'message': "{}.opening_times is invalide. Missing days.".format(location['name'])}, 400)
+		# 		break
 			
-			elif (not opening_time.get('closed') and (opening_time['closed']!=False)):
-				print("Error opeing_time['closed']")
-				response = ({'message': "{}.opening_times is invalide. Missing data".format(location['name'])}, 400)
-				break
+		# 	elif (not opening_time.get('closed') and (opening_time['closed']!=False)):
+		# 		print("Error opeing_time['closed']")
+		# 		response = ({'message': "{}.opening_times is invalide. Missing data".format(location['name'])}, 400)
+		# 		break
 			
-			else:
-				print("createing an opening time for = {}".format(opening_time))
-				# add valid opening time:
-				if opening_time['closed']:
-					opening_record = OpeningTime(
-						days=opening_time['days'],
-						closed=opening_time['closed']
-					)
+		# 	else:
+		# 		print("createing an opening time for = {}".format(opening_time))
+		# 		# add valid opening time:
+		# 		if opening_time['closed']:
+		# 			opening_record = OpeningTime(
+		# 				days=opening_time['days'],
+		# 				closed=opening_time['closed']
+		# 			)
 
-				else:
-					opening_record = OpeningTime(
-						days=opening_time['days'],
-						opening=opening_time['opening'],
-						closing=opening_time['closing'],
-						closed=False 
-						)
+		# 		else:
+		# 			opening_record = OpeningTime(
+		# 				days=opening_time['days'],
+		# 				opening=opening_time['opening'],
+		# 				closing=opening_time['closing'],
+		# 				closed=False 
+		# 				)
 
-				opening_time_records.append(opening_record)
+		# 		opening_time_records.append(opening_record)
 
+		opening_time_records = build_opening_times(opening_times_list=location_data['openingTimes'])
 		location.openingTimes = opening_time_records
 
 		
@@ -105,6 +106,54 @@ def location_create(request):
 
 
 	return response
+
+
+
+def build_opening_times(opening_times_list):
+	'''
+
+
+	'''
+
+	# add the opening time sub documents:
+	opening_time_records = list()
+	for opening_time in opening_times_list:
+		print("opening_time= {}".format(opening_time))
+
+		# check required data days and closed:
+		if not opening_time.get('days'):
+			print("Error opeing_time['days']")
+			response = ({'message': "{}.opening_times is invalide. Missing days.".format(location['name'])}, 400)
+			break
+		
+		elif (not opening_time.get('closed') and (opening_time['closed']!=False)):
+			print("Error opeing_time['closed']")
+			response = ({'message': "{}.opening_times is invalide. Missing data".format(location['name'])}, 400)
+			break
+		
+		else:
+			print("createing an opening time for = {}".format(opening_time))
+			# add valid opening time:
+			if opening_time['closed']:
+				opening_record = OpeningTime(
+					days=opening_time['days'],
+					closed=opening_time['closed']
+				)
+
+			else:
+				opening_record = OpeningTime(
+					days=opening_time['days'],
+					opening=opening_time['opening'],
+					closing=opening_time['closing'],
+					closed=False 
+					)
+
+			opening_time_records.append(opening_record)
+
+
+	return opening_time_records
+
+
 
 def convert_object_ids(document):
 	'''
@@ -166,11 +215,40 @@ def location_read(request, locationid=None):
 
 def location_update(request, locationid):
 
-	location_data = {
-		'locationid': locationid
-	}
+	if locationid is None:
+		response = ({"message": "locationid is required"}, 404)
 
-	return (location_data, 200)
+	else:
+
+		try:
+
+			location = Location.objects(id=locationid).get()
+
+			new_data = request.get_json()
+
+			location.name = new_data['name']
+			location.address = new_data['address']
+			location.facilities = new_data['facilities']
+			location.coords = [new_data['lng'], new_data['lat']]
+
+			opening_time_records = build_opening_times(opening_times_list=location_data['openingTimes'])
+			location.openingTimes = opening_time_records
+
+			location.save()
+
+			location_data = convert_object_ids(document=location)
+
+			response = (location_data, 200)
+
+		except Exception as e:
+
+			err_msg = "{} is not a valid id for a locaiton.".format(locationid)
+			# err_msg += e
+
+			response = (err_msg, 404)
+
+
+	return response 
 
 
 def location_delete(request, locationid):

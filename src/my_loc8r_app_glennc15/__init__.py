7c6ddcb20a0251f62ed1a4787cad1627590607e4
Glenn <gcrosby15@gmail.com>
2023-05-21 16:14:13 +0700

    update __init__.py

---
+++ src/my_loc8r_app_glennc15/__init__.py
@@ -0,0 +1,3 @@
+from my_loc8r_app_glennc15 import create_app
+
+application = create_app()
--- src/my_loc8r_app_glennc15/wsgi.py
+++ src/my_loc8r_app_glennc15/wsgi.py
@@ -1,3 +1,3 @@
-from project import create_app
+from my_loc8r_app_glennc15 import create_app
 
 application = create_app()
